877ccbe9d1c5300c3e094647816fda3ad355b1be
Kawsar Ali <59552845+Kawsar07@users.noreply.github.com>
2022-06-26 11:59:14 +0600

    home page All Products

--- lib/api_controller/allProduct_controller.dart
+++ lib/api_controller/allProduct_controller.dart
@@ -50,6 +50,39 @@
 // }
 
 
+// import 'package:dio/dio.dart';
+// import 'package:flutter_ecommerce_task/api_service/all_product.dart';
+// import 'package:flutter_ecommerce_task/api_service/products_details.dart';
+// import 'package:flutter_ecommerce_task/model/alProductsBd.dart';
+// import 'package:flutter_ecommerce_task/model/productDetails.dart';
+//
+// import 'package:get/get.dart';
+//
+// class AllProductController extends GetxController with StateMixin<AlProductsBd> {
+//
+//   final Dio _dio = Dio();
+//   final AllProductRemoteService _remoteService = AllProductRemoteService(Dio());
+//
+//   var addDataDoc = <String, dynamic>{};
+//   List<AlProductsBd> dataList = <AlProductsBd>[];
+//
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     getIn();
+//   }
+//   Future<void> getIn() async {
+//     change(null, status: RxStatus.loading());
+//     try {
+//       final dis = await _remoteService.getAllPosts();
+//       change(dis, status: RxStatus.success());
+//     } catch (error) {
+//       change(null, status: RxStatus.error());
+//     }
+//   }
+// }
+
+
 import 'package:dio/dio.dart';
 import 'package:flutter_ecommerce_task/api_service/all_product.dart';
 import 'package:flutter_ecommerce_task/api_service/products_details.dart';
@@ -57,9 +90,12 @@ import 'package:flutter_ecommerce_task/model/alProductsBd.dart';
 import 'package:flutter_ecommerce_task/model/productDetails.dart';
 
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class AllProductController extends GetxController with StateMixin<AlProductsBd> {
-
+  static const _pageSize = 20;
+  final PagingController<int, AlProductsBd> _pagingController =
+  PagingController(firstPageKey: 0);
   final Dio _dio = Dio();
   final AllProductRemoteService _remoteService = AllProductRemoteService(Dio());
 
